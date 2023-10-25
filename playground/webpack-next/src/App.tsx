0807cc98908cfe7fcf8e1d215e7548e513e982da
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2023-10-25 09:26:49 -0500

    chore: update playgrounds ico (#133)

--- playground/webpack-next/src/App.tsx
+++ playground/webpack-next/src/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <>
       <Head>
+        <link rel="icon" type="image/png" href="/logo.png" />
         <title>Open Editor</title>
       </Head>
       <div>
