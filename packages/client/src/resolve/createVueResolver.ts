74b451da3692e1180aebcef13b73f868366a5ba7
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2023-12-06 19:20:43 +0800

    fix: Make screen unscrollable (#167)
    * fix: Make screen unscrollable
    
    * feat: looser extension

--- packages/client/src/resolve/createVueResolver.ts
+++ packages/client/src/resolve/createVueResolver.ts
@@ -160,7 +160,7 @@ function getIsSourceResult<T = any>(
   return false;
 }
 
-const nameRE = /([^/.]+)\.(vue|jsx|tsx|js|ts|mjs|cjs)$/;
+const nameRE = /([^/.]+)\.[^.]+$/;
 function getNameByFile(file = '') {
   return file.match(nameRE)?.[1];
 }
