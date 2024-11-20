3571ab17cf8635a866f40745fa18f3edef8db7de
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2024-11-20 20:37:41 +0800

    chore: Optimize rendering process (#269)

--- packages/client/src/inspector/inspectorState.ts
+++ packages/client/src/inspector/inspectorState.ts
@@ -1,7 +1,6 @@
 export interface InspectorState {
   isEnable: boolean;
   isActive: boolean;
-  isRending: boolean;
   isTreeOpen: boolean;
   activeEl: HTMLElement | null;
   prevActiveEl: HTMLElement | null;
@@ -10,7 +9,6 @@ export interface InspectorState {
 export const inspectorState: InspectorState = {
   isEnable: false,
   isActive: false,
-  isRending: false,
   isTreeOpen: false,
   activeEl: null,
   prevActiveEl: null,
