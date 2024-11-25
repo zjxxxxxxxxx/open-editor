17f7310cda6a51270c4639b684ce00d7d7daa247
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2024-11-25 20:15:38 +0800

    chore: Set up tasks

--- scripts/tasks.ts
+++ scripts/tasks.ts
@@ -7,11 +7,18 @@ const TASKS_PATH = resolve('.codesandbox/tasks.json');
 const taskJson = readjson(TASKS_PATH);
 const names = readdirSync(resolve('playground'));
 
+let port = 4000;
 names.flatMap((name) => {
   const task = `@playground/${name}`;
   taskJson.tasks[name] = {
-    name: `Run ${task}`,
+    name: `Preview ${task}`,
     command: `pnpm --filter ${task} dev`,
+    runAtStart: true,
+    preview: {
+      port: port++,
+      prLink: 'direct',
+    },
   };
 });
+
 writejson(TASKS_PATH, taskJson);
