637012622e1302612978c65c19041c4110e34ff4
zjxxxxxxxxx <43126836+zjxxxxxxxxx@users.noreply.github.com>
2024-11-25 19:30:36 +0800

    f

--- scripts/tasks.ts
+++ scripts/tasks.ts
@@ -7,11 +7,17 @@ const TASKS_PATH = resolve('.codesandbox/tasks.json');
 const taskJson = readjson(TASKS_PATH);
 const names = readdirSync(resolve('playground'));
 
+let port = 3000;
 names.flatMap((name) => {
   const task = `@playground/${name}`;
   taskJson.tasks[name] = {
     name: `Run ${task}`,
     command: `pnpm --filter ${task} dev`,
+    preview: {
+      port: ++port,
+      prLink: 'direct',
+    },
   };
 });
+
 writejson(TASKS_PATH, taskJson);
